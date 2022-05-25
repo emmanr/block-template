a2a82e8ddd64aaaa6bcbcbcc135bac42049e96c9
emmanr <emman.info22@gmail.com>
2022-05-25 22:45:13 +0800

    Updated block author and description

--- block-boilerplate.php
+++ block-boilerplate.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Block Boilerplate
- * Description:       Example static block scaffolded with Create Block tool.
+ * Description:       Boilerplate for creating a block
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Emman Ruaza
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       block-boilerplate
